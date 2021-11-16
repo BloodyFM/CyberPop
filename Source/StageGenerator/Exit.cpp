05c94526679efe5e94f516c10e6c165b137e99b7
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2021-11-16 18:15:10 +0100

    fix exit

--- Source/StageGenerator/Exit.cpp
+++ Source/StageGenerator/Exit.cpp
@@ -39,11 +39,5 @@ void AExit::Tick(float DeltaTime)
 void AExit::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	ACreature* Creature = Cast<ACreature>(OtherActor);
-	if (Creature)
-		OpenNextLevel();
-}
-
-void AExit::OpenNextLevel_Implementation()
-{
+	
 }
--- Source/StageGenerator/Exit.h
+++ Source/StageGenerator/Exit.h
@@ -33,6 +33,4 @@ public:
 		virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 			int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "LevelEnd")
-	void OpenNextLevel();
 };
