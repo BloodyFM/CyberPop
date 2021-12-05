ee6b4139b81fdbe468d78a5dd3369ca41e58f17a
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2021-12-05 17:57:43 +0100

    made main class and adjusted camera

---
+++ Source/StageGenerator/Main1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Creature.h"
+#include "Main1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STAGEGENERATOR_API AMain1 : public ACreature
+{
+	GENERATED_BODY()
+	
+};
