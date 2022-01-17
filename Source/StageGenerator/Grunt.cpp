8dc5064423b263b3d626abcc8e85eda6faa7e3a0
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2022-01-17 16:25:19 +0100

    added the basics to grunt

--- Source/StageGenerator/Grunt.cpp
+++ Source/StageGenerator/Grunt.cpp
@@ -1,5 +1,47 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Components/SphereComponent.h"
+#include "AIController.h"
+#include "AIModule.h"
 #include "Grunt.h"
 
+
+AGrunt::AGrunt()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AggroSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AggroSphere"));
+	AggroSphere->SetupAttachment(GetRootComponent());
+	AggroSphere->InitSphereRadius(600.f);
+}
+
+void AGrunt::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AIController = Cast<AAIController>(GetController());
+
+	AggroSphere->OnComponentBeginOverlap.AddDynamic(this, &AGrunt::AggroSphereOnOverlapBegin);
+
+}
+
+void AGrunt::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AGrunt::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AGrunt::AggroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+
+	}
+}
--- Source/StageGenerator/Grunt.h
+++ Source/StageGenerator/Grunt.h
@@ -6,12 +6,57 @@
 #include "Creature.h"
 #include "Grunt.generated.h"
 
-/**
- * 
- */
+
+UENUM(BlueprintType)
+enum class EGruntMovementStatus :uint8
+{
+	EMS_Idle			UMETA(DisplayName = "Idle"),
+	EMS_Retreat			UMETA(DisplayName = " Retreat"),
+	EMS_Attacking		UMETA(DisplayName = "Attacking"),
+
+	EMS_MAX				UMETA(DisplayName = "DefaultMAX")
+};
+
 UCLASS()
 class STAGEGENERATOR_API AGrunt : public ACreature
 {
 	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AGrunt();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	EGruntMovementStatus GruntMovementStatus;
+
+	FORCEINLINE void SetLeaperMovementStatus(EGruntMovementStatus Status) { GruntMovementStatus = Status; }
+
+	// Sphere that will aggro the ai if the player overlaps
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	class USphereComponent* AggroSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	class AAIController* AIController;
+
+	// Movement values
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float MovementSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float TurnRate;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+		virtual void AggroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 	
 };
