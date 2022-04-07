e5bad16698bebff68a65f9787355b6d534c1a7c5
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2022-04-07 18:03:18 +0200

    boss 1 basic setup

---
+++ Source/StageGenerator/Boss1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Creature.h"
+#include "Boss1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STAGEGENERATOR_API ABoss1 : public ACreature
+{
+	GENERATED_BODY()
+	
+};
