67b6f56865fdf856315d59fb859e8d15f57d2495
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2022-04-18 22:33:43 +0200

    Added a bunch of hud elements, some mc2 code and death menu

--- Source/StageGenerator/Main2.cpp
+++ Source/StageGenerator/Main2.cpp
@@ -3,16 +3,26 @@
 
 #include "Main2.h"
 #include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Components/SceneComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Engine/StaticMeshSocket.h"
 #include "Engine/SkeletalMeshSocket.h"
+#include "Kismet/KismetMathLibrary.h"
 
 AMain2::AMain2()
 {
+
+	LockOnSphere = CreateDefaultSubobject<USphereComponent>(TEXT("LockOnSphere"));
+	LockOnSphere->SetupAttachment(GetRootComponent());
+
 	FistBoxL = CreateDefaultSubobject<UBoxComponent>(TEXT("FistBoxL"));
 	FistBoxR = CreateDefaultSubobject<UBoxComponent>(TEXT("FistBoxR"));
+
+
+	MagnetSpeed = 400.f;
+	MovementSpeedTank = 600.f;
 
 	FistBoxL->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("LeftFistSocket"));
 	FistBoxR->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("RightFistSocket"));
@@ -22,9 +32,99 @@ void AMain2::BeginPlay()
 {
 	Super::BeginPlay();
 
+	GetCharacterMovement()->MaxWalkSpeed = MovementSpeedTank; // Sets Movement Speed
+
+	LockOnSphere->OnComponentBeginOverlap.AddDynamic(this, &AMain2::LockOnSphereOverlapBegin);
+	LockOnSphere->OnComponentEndOverlap.AddDynamic(this, &AMain2::LockOnSphereOverlapEnd);
+
+	LockOnList.Empty();
+
 }
 
 void AMain2::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AMain2::LockOnSphereOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		ACreature* Creature = Cast<ACreature>(OtherActor);
+		if (Creature)
+		{
+			if (!Creature->bIsMainCharacter)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Added possible target."));
+				LockOnList.Add(Creature);
+			}
+		}
+	}
+}
+
+void AMain2::LockOnSphereOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor)
+	{
+		ACreature* Creature = Cast<ACreature>(OtherActor);
+		if (Creature)
+		{
+			LockOnList.Remove(Creature);
+		}
+	}
+}
+
+void AMain2::RemoveLockOnTarget(ACreature* Creature)
+{
+	if (LockOnList.Contains(Creature))
+	{
+		LockOnList.Remove(Creature);
+	}
+}
+
+ACreature* AMain2::FindBestLockOnTarget()
+{
+	FRotator ForwardRotator = GetActorRotation();
+	ACreature* BestMatch = nullptr;
+
+	//FRotator LookAtYaw = GetLookAtRotationYaw(Bestmatch->GetActorLocation());
+
+	float CurrentAngle = 1000;
+
+	for (int i = 0; i < LockOnList.Num(); i++)
+	{
+		float NewAngle;
+		FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), LockOnList[i]->GetActorLocation());
+		FRotator YawLookAtRotation = FRotator(0.f, LookAtRotation.Yaw, 0.f);
+
+		NewAngle = YawLookAtRotation.Yaw - ForwardRotator.Yaw;
+
+		if (NewAngle > 0)
+		{
+			NewAngle = -NewAngle;
+
+		}
+		if (CurrentAngle > NewAngle)
+		{
+			CurrentAngle = NewAngle;
+			BestMatch = LockOnList[i];
+		}
+
+	}
+	return BestMatch;
+}
+
+void AMain2::LockOnNotify()
+{
+	LockOnTarget = FindBestLockOnTarget();
+	GetCharacterMovement()->MaxWalkSpeed = 200.f;
+}
+
+FRotator AMain2::GetLookAtRotationYaw(FVector Target)
+{
+	FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), Target);
+	FRotator YawLookAtRotation = FRotator(0.f, LookAtRotation.Yaw, 0.f);
+	return YawLookAtRotation;
 }
--- Source/StageGenerator/Main2.h
+++ Source/StageGenerator/Main2.h
@@ -24,6 +24,28 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attack")
 		class UBoxComponent* FistBoxR;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+		float MovementSpeedTank;
+
+	//Lock on
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "LockOn")
+		class USphereComponent* LockOnSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "LockOn")
+		TArray<ACreature*> LockOnList;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "LockOn")
+		ACreature* LockOnTarget = nullptr;
+	UFUNCTION()
+		void RemoveLockOnTarget(ACreature* Creature);
+	UFUNCTION()
+		FRotator GetLookAtRotationYaw(FVector Target);
+	UFUNCTION()
+		ACreature* FindBestLockOnTarget();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Movement")
+		float MagnetSpeed;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -32,4 +54,14 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+		virtual void LockOnSphereOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		virtual void LockOnSphereOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintCallable)
+		void LockOnNotify();
 };
