9bd3a888e8e6da6915009e8489dee35536f801ab
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2022-04-27 18:33:47 +0200

    more boss 2 setup + minifix
    i believe i have managed to stop grunt movement when player dies so should hopefully be no errors

--- Source/StageGenerator/Boss2.cpp
+++ Source/StageGenerator/Boss2.cpp
@@ -2,4 +2,49 @@
 
 
 #include "Boss2.h"
+#include "AIController.h"
+#include "AIModule.h"
+#include "Kismet/KismetMathLibrary.h"
 
+
+ABoss2::ABoss2()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ABoss2::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AIController = Cast<AAIController>(GetController());
+
+}
+
+void ABoss2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bInterpToTarget && CombatTarget)
+	{
+		FRotator LookAtYaw = GetLookAtRotationYaw(CombatTarget->GetActorLocation());
+		FRotator InterpRotation = FMath::RInterpTo(GetActorRotation(), LookAtYaw, DeltaTime, TurnRate);
+
+		SetActorRotation(InterpRotation);
+	}
+}
+
+FRotator ABoss2::GetLookAtRotationYaw(FVector Target)
+{
+	FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), Target);
+	FRotator YawLookAtRotation = FRotator(0.f, LookAtRotation.Yaw, 0.f);
+	return YawLookAtRotation;
+}
+
+void ABoss2::Aggro(ACreature* target)
+{
+	AIController = Cast<AAIController>(GetController());
+	CombatTarget = target;
+	bInterpToTarget = true;
+}
